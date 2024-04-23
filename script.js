f710ccc7489b48abb26de6ee9019e2ef38c05839
Will Garvey <willmgarvey@gmail.com>
2024-04-23 14:48:50 -0500

    Decreased size of game to 12 tiles and 6 matches

--- script.js
+++ script.js
@@ -39,6 +39,36 @@ let restartGame = document.querySelector(".mainMenu");
 // Flip the cards for show after the cards are assigned
 flipCardsWithDelay();
 
+function restart() {
+  resetButton.classList.add("hidden");
+  playersTurn.classList.add("hidden");
+  scoreboardPlayerOne.classList.add("hidden");
+  scoreboardPlayerTwo.classList.add("hidden");
+  choice.classList.remove("hidden");
+  singlePlayerMode.classList.remove("hidden");
+  twoPlayerMode.classList.remove("hidden");
+  currentPlayer = "Player 1"
+  document.querySelector('#currentPlayer').textContent = playerOne
+  gameOver = false;
+  gameMode = null;
+  matchedPairs = 0;
+  playerOneScore = 0;
+  playerTwoScore = 0;
+  document.getElementById("playerOneScore").textContent = playerOneScore;
+  document.getElementById("playerTwoScore").textContent = playerTwoScore;
+  cards.forEach((card) => { // this is a loop that goes through each card. forEach card, it removes
+    // card.removeEventListener("click", gameLogic);
+    // TODO: No need to remove card types here. Looks more realistic card.dataset.cardType = ""; // the cardType data attribute (sets it to an empty string)
+    card.classList.remove("matched"); // and removes the "matched" class. This effectively resets
+    card.classList.remove("flipped"); // the state of each card "erasing" what type of card it is and whether it's been matched or not.
+  }); // finally, card.classList.remove("flipped") removes the "flipped" status of the cards, so when the 
+  // Restart Game button is pressed, the cards shuffle and get flipped to their front side (black side)
+  // TODO: Do not need to shuffle here assignCards(); // finally, calls assignCards function which assigns new card types to each card in a shuffled order
+  clickable = false;
+  firstCard = null;
+  secondCard = null; // TODO: Semicolon missing here
+}
+
 // event listeners to select either Single Player or Two Players
 singlePlayerMode.addEventListener("click", () => {
   startGame(gameMode = "single player");
@@ -51,6 +81,7 @@ resetButton.addEventListener("click", restart);
 //Functions
 //Flips card in random streaks during the main menu
 async function flipCardsWithDelay() {
+  assignCards();
   for (let i = 0; i < cards.length; i++) {
     if (!mainMenu){
       break;
@@ -70,7 +101,7 @@ async function flipCardsWithDelay() {
     if (i === cards.length/2-1){
       i=0;
       await new Promise(resolve => setTimeout(resolve, 1000)); // Half a second dela
-      assignCards();
+
     }
   }
 }
@@ -246,6 +277,8 @@ function startGame(gameMode) {
     });
   });
 }
+
+
 
 
 
